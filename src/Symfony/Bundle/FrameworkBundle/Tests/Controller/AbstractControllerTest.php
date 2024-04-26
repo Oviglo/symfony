0191c7167254196b1dd440ecc239e6c53f63f490
Oviglo <ovigne.loic@gmail.com>
2024-04-26 10:31:50 +0200

    new method isSubmittedFormValid

--- src/Symfony/Bundle/FrameworkBundle/Tests/Controller/AbstractControllerTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Controller/AbstractControllerTest.php
@@ -644,4 +644,30 @@ class AbstractControllerTest extends TestCase
 
         $this->assertSame('</style.css>; rel="preload"; as="stylesheet",</script.js>; rel="preload"; as="script"', $response->headers->get('Link'));
     }
+
+    public function testIsSubmittedFormValidReturnTrue()
+    {
+        $form = $this->getMockBuilder(FormInterface::class)->getMock();
+        $form->expects($this->once())->method('isSubmitted')->willReturn(true);
+        $form->expects($this->once())->method('isValid')->willReturn(true);
+
+        $request = new Request();
+
+        $controller = $this->createController();
+
+        $this->assertTrue($controller->isSubmittedFormValid($form, $request));
+    }
+
+    public function testIsSubmittedFormValidReturnFalse()
+    {
+        $form = $this->getMockBuilder(FormInterface::class)->getMock();
+        $form->expects($this->once())->method('isSubmitted')->willReturn(true);
+        $form->expects($this->once())->method('isValid')->willReturn(false);
+
+        $request = new Request();
+
+        $controller = $this->createController();
+
+        $this->assertFalse($controller->isSubmittedFormValid($form, $request));
+    }
 }
